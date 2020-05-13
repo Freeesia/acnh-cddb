0adbd874cf5b7680a963f07cc02364f4607104e2
Freesia <freesia@studiofreesia.com>
2020-05-13 23:38:45 +0900

    必要な情報の抜出

--- Test/AnalysisImage/Program.cs
+++ Test/AnalysisImage/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -8,8 +10,11 @@ namespace AnalysisImage
 {
     class Program
     {
-        private static Regex authorPattern = new Regex(@"^MA-\d{4}-\d{4}-\d{4}$");
-        private static Regex designPattern = new Regex(@"^MO-[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{4}$");
+        private static Rectangle titleLine = new Rectangle(400, 140, 400, 0);
+        private static Rectangle authorNameLine = new Rectangle(150, 450, 250, 0);
+        private static Rectangle islandNameLine = new Rectangle(150, 500, 250, 0);
+        private static Regex authorIdPattern = new Regex(@"^MA-\d{4}-\d{4}-\d{4}$");
+        private static Regex designIdPattern = new Regex(@"^MO-[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{4}$");
 
         static async Task Main(string[] args)
         {
@@ -17,11 +22,56 @@ namespace AnalysisImage
             var client = ImageAnnotatorClient.Create();
             // Load the image file into memory
             var image = Image.FromUri("https://pbs.twimg.com/media/EXVpohEUEAAl4BM?format=jpg");
-            var res = await client.DetectTextAsync(image);
-            var author = res.Select(r => r.Description).FirstOrDefault(t => authorPattern.IsMatch(t));
-            Console.WriteLine($"作者：{author}");
-            var design = res.Select(r => r.Description).FirstOrDefault(t => designPattern.IsMatch(t));
-            Console.WriteLine($"デザイン：{design}");
+
+            var title = string.Empty;
+            var islandName = string.Empty;
+            var authorName = string.Empty;
+            var authorId = string.Empty;
+            var designId = string.Empty;
+
+            var textAnnotations = await client.DetectTextAsync(image);
+            foreach (var t in textAnnotations.Skip(1))
+            {
+                var rect = ToRect(t.BoundingPoly.Vertices);
+                if (rect.IntersectsWith(titleLine))
+                {
+                    title += t.Description;
+                }
+                else if (rect.IntersectsWith(authorNameLine))
+                {
+                    authorName += t.Description;
+
+                }
+                else if (rect.IntersectsWith(islandNameLine))
+                {
+                    islandName += t.Description;
+                }
+                else if (authorIdPattern.IsMatch(t.Description))
+                {
+                    authorId = t.Description;
+                }
+                else if (designIdPattern.IsMatch(t.Description))
+                {
+                    designId = t.Description;
+                }
+            }
+
+            Console.WriteLine("タイトル: " + title);
+            Console.WriteLine("島名: " + islandName);
+            Console.WriteLine("作者: " + authorName);
+            Console.WriteLine("作者ID: " + authorId);
+            Console.WriteLine("デザインID: " + designId);
+        }
+
+        private static Point ToPoint(Vertex v) => new Point(v.X, v.Y);
+
+        private static Rectangle ToRect(IEnumerable<Vertex> box)
+        {
+            var left = box.Min(v => v.X);
+            var right = box.Max(v => v.X);
+            var top = box.Min(v => v.Y);
+            var bottom = box.Max(v => v.Y);
+            return Rectangle.FromLTRB(left, top, right, bottom);
         }
     }
 }
