2a13de3ce59bd8bc6ec9caae93fb00444c07bcaa
Freesia <freesia@studiofreesia.com>
2020-05-17 04:41:31 +0900

    ドミナントカラーの抽出

--- Test/AnalysisImage/Program.cs
+++ Test/AnalysisImage/Program.cs
@@ -4,12 +4,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using ColorMine.ColorSpaces;
+using ColorMine.ColorSpaces.Comparisons;
 using Google.Cloud.Vision.V1;
 
 namespace AnalysisImage
 {
     class Program
     {
+        private static Rgb backColor = new Rgb() { R = 243, G = 241, B = 226 };
+        private static (string name, Rgb color)[] colors = new[]
+        {
+            ("red", new Rgb() { R = 255 }),
+            ("pink", new Rgb() { R = 255, G = 192, B = 203 }),
+            ("orange", new Rgb() { R = 239, G = 129, B = 15 }),
+            ("yellow", new Rgb() { R = 255, G = 255, B = 0 }),
+            ("green", new Rgb() { G = 255 }),
+            ("blue", new Rgb() { B = 255 }),
+            ("purple", new Rgb() { R = 204, G = 0, B = 255 }),
+            ("brown", new Rgb() { R = 158, G = 117, B = 58 }),
+            ("white", new Rgb() { R = 255, G = 255, B = 255 }),
+            ("black", new Rgb()),
+        };
+        private static IColorSpaceComparison colorComparison = new CieDe2000Comparison();
         private static Rectangle titleLine = new Rectangle(400, 140, 400, 0);
         private static Rectangle authorNameLine = new Rectangle(150, 450, 250, 0);
         private static Rectangle islandNameLine = new Rectangle(150, 500, 250, 0);
@@ -21,7 +38,7 @@ namespace AnalysisImage
             // Instantiates a client
             var client = ImageAnnotatorClient.Create();
             // Load the image file into memory
-            var image = Image.FromUri("https://pbs.twimg.com/media/EXVpohEUEAAl4BM?format=jpg");
+            var image = Image.FromUri("https://pbs.twimg.com/media/EXuQTz5UwAE-LiI?format=jpg");
 
             var title = string.Empty;
             var islandName = string.Empty;
@@ -61,9 +78,53 @@ namespace AnalysisImage
             Console.WriteLine("作者: " + authorName);
             Console.WriteLine("作者ID: " + authorId);
             Console.WriteLine("デザインID: " + designId);
+
+            var context = new ImageContext()
+            {
+                ProductSearchParams = new ProductSearchParams()
+                {
+                    BoundingPoly = new BoundingPoly()
+                    {
+                        Vertices =
+                        {
+                            new Vertex() { X = 997, Y = 328 },
+                            new Vertex() { X = 1150, Y = 328 },
+                            new Vertex() { X = 997, Y = 482 },
+                            new Vertex() { X = 1150, Y = 482 },
+                        }
+                    }
+                }
+            };
+            var response = await client.DetectImagePropertiesAsync(image, context);
+            foreach (var color in response.DominantColors.Colors.Where(c => c.Score > 0.05).OrderByDescending(c => c.PixelFraction))
+            {
+                var mineColor = new Rgb()
+                {
+                    R = color.Color.Red,
+                    G = color.Color.Green,
+                    B = color.Color.Blue,
+                };
+                if (IsBackColor(mineColor))
+                {
+                    Console.WriteLine($"{color.PixelFraction}: {color.Score}: {mineColor.To<Hex>().Code} (BackColor)");
+                }
+                else
+                {
+                    var apColor = GetApproximateColor(mineColor);
+                    Console.WriteLine($"{color.PixelFraction}: {color.Score}: {mineColor.To<Hex>().Code} ({apColor})");
+                }
+            }
         }
 
-        private static Point ToPoint(Vertex v) => new Point(v.X, v.Y);
+        private static bool IsBackColor(Rgb color)
+             => Math.Abs(color.R - backColor.R) < 5 &&
+                Math.Abs(color.G - backColor.G) < 10 &&
+                Math.Abs(color.B - backColor.B) < 20;
+        private static string GetApproximateColor(Rgb color)
+            => colors.Select(c => (c.name, d: c.color.Compare(color, colorComparison)))
+                .OrderBy(c => c.d)
+                .First()
+                .name;
 
         private static Rectangle ToRect(IEnumerable<Vertex> box)
         {
