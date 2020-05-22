130d6f3b39b580feb7d9494660bc9ed5b39c9329
Freesia <freesia@studiofreesia.com>
2020-05-23 05:06:08 +0900

    適当にGCP呼び出し

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,21 @@
-import * as functions from "firebase-functions";
+import { https } from "firebase-functions";
+import { ImageAnnotatorClient } from "@google-cloud/vision";
+import { assertIsDefined } from "./assert";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+async function analyzeImageUrl(url: string) {
+  const client = new ImageAnnotatorClient();
+  const [res] = await client.textDetection("https://pbs.twimg.com/media/EXuQTz5UwAE-LiI?format=jpg");
+  const textAnnotations = res.textAnnotations;
+  assertIsDefined(textAnnotations);
+  for (const t of textAnnotations.slice(1)) {
+    console.log(t);
+  }
+}
+
+export const helloWorld = https.onRequest((request, response) => {
+  // analyzeImageUrl("");
   response.send("Hello from Firebase!");
 });
