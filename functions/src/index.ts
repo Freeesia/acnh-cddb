b42fddf0770cfe254890fc1c5a82a866c3960a3f
Freesia <freesia@studiofreesia.com>
2020-05-30 03:17:32 +0900

    実装整理

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,22 +2,21 @@ import { https } from "firebase-functions";
 import { initializeApp } from "firebase-admin";
 import { ImageAnnotatorClient, protos } from "@google-cloud/vision";
 import { assertIsDefined } from "./assert";
-import { boxLine } from "intersects";
 import _ from "lodash";
 import diff, { RGBColor } from "color-diff";
-import { red, pink, orange, yellow, green, blue, purple, brown, white, black } from "color-name";
 import convert from "color-convert";
-import IVertex = protos.google.cloud.vision.v1.IVertex;
+import { red, pink, orange, yellow, green, blue, purple, brown, white, black } from "color-name";
 import IImageContext = protos.google.cloud.vision.v1.IImageContext;
 import IColor = protos.google.type.IColor;
 import { HttpsError } from "firebase-functions/lib/providers/https";
+import { toRGBColor, Info, toRect, Line, intersect } from "./utility";
 initializeApp();
 const client = new ImageAnnotatorClient();
 
-const titleLine = { x1: 400, y1: 140, x2: 800, y2: 140 };
-const authorNameLine = { x1: 150, y1: 450, x2: 400, y2: 450 };
-const islandNameLine = { x1: 150, y1: 500, x2: 400, y2: 500 };
-const designTypeLine = { x1: 900, y1: 500, x2: 1100, y2: 500 };
+const titleLine: Line = { x1: 400, y1: 140, x2: 800, y2: 140 };
+const authorNameLine: Line = { x1: 150, y1: 450, x2: 400, y2: 450 };
+const islandNameLine: Line = { x1: 150, y1: 500, x2: 400, y2: 500 };
+const designTypeLine: Line = { x1: 900, y1: 500, x2: 1100, y2: 500 };
 const authorIdPattern = /^MA-\d{4}-\d{4}-\d{4}$/;
 const designIdPattern = /^MO-[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{4}$/;
 
@@ -35,6 +34,15 @@ const mainColors: RGBColor[] = [
   toRGBColor(black),
 ];
 
+function toMainColor(color?: IColor | null): string {
+  assertIsDefined(color);
+  assertIsDefined(color.red);
+  assertIsDefined(color.green);
+  assertIsDefined(color.blue);
+  const c = diff.closest({ R: color.red, G: color.green, B: color.blue }, mainColors, backColor);
+  return convert.rgb.keyword([c.R, c.G, c.B]);
+}
+
 const dominantContext: IImageContext = {
   productSearchParams: {
     boundingPoly: {
@@ -48,61 +56,7 @@ const dominantContext: IImageContext = {
   },
 };
 
-interface info {
-  url: string;
-  title: string;
-  designId: string;
-  dominantColor: string;
-  designType: string;
-  authorName: string;
-  authorId: string;
-  islandName: string;
-}
-
-interface rect {
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-}
-
-function toRGBColor(c: [number, number, number]): RGBColor {
-  return { R: c[0], G: c[1], B: c[2] };
-}
-
-function toMainColor(color?: IColor | null): string {
-  assertIsDefined(color);
-  const c = diff.closest({ R: color.red!, G: color.green!, B: color.blue! }, mainColors, backColor);
-  return convert.rgb.keyword([c.R, c.G, c.B]);
-}
-
-function toRect(vertices?: IVertex[] | null): rect {
-  assertIsDefined(vertices);
-  const left = _(vertices)
-    .map((v) => v.x)
-    .min();
-  const right = _(vertices)
-    .map((v) => v.x)
-    .max();
-  const top = _(vertices)
-    .map((v) => v.y)
-    .min();
-  const bottom = _(vertices)
-    .map((v) => v.y)
-    .max();
-  assertIsDefined(left);
-  assertIsDefined(right);
-  assertIsDefined(top);
-  assertIsDefined(bottom);
-  return {
-    x: left,
-    y: top,
-    w: right - left,
-    h: bottom - top,
-  };
-}
-
-async function analyzeImageUrl(url: string): Promise<info> {
+async function analyzeImageUrl(url: string): Promise<Info> {
   let title = "";
   let authorName = "";
   let islandName = "";
@@ -115,14 +69,14 @@ async function analyzeImageUrl(url: string): Promise<info> {
     const textAnnotations = res.textAnnotations;
     assertIsDefined(textAnnotations);
     for (const t of textAnnotations.slice(1)) {
-      const { x, y, w, h } = toRect(t.boundingPoly?.vertices);
-      if (boxLine(x, y, w, h, titleLine.x1, titleLine.y1, titleLine.x2, titleLine.y2)) {
+      const r = toRect(t.boundingPoly?.vertices);
+      if (intersect(r, titleLine)) {
         title += t.description;
-      } else if (boxLine(x, y, w, h, authorNameLine.x1, authorNameLine.y1, authorNameLine.x2, authorNameLine.y2)) {
+      } else if (intersect(r, authorNameLine)) {
         authorName += t.description;
-      } else if (boxLine(x, y, w, h, islandNameLine.x1, islandNameLine.y1, islandNameLine.x2, islandNameLine.y2)) {
+      } else if (intersect(r, islandNameLine)) {
         islandName += t.description;
-      } else if (boxLine(x, y, w, h, designTypeLine.x1, designTypeLine.y1, designTypeLine.x2, designTypeLine.y2)) {
+      } else if (intersect(r, designTypeLine)) {
         designType += t.description;
       } else if (t.description && authorIdPattern.test(t.description)) {
         authorId = t.description;
@@ -141,8 +95,8 @@ async function analyzeImageUrl(url: string): Promise<info> {
       imageContext: dominantContext,
     } as any);
     const color = _(res.imagePropertiesAnnotation?.dominantColors?.colors ?? [])
-      .filter((c) => (c.score ?? 0) > 0.05)
-      .orderBy((c) => c.pixelFraction)
+      .filter(c => (c.score ?? 0) > 0.05)
+      .orderBy(c => c.pixelFraction)
       .first();
     assertIsDefined(color);
     dominantColor = toMainColor(color.color);
@@ -164,7 +118,7 @@ export const helloWorld = https.onRequest(async (request, response) => {
   if (!urls) {
     throw new HttpsError("invalid-argument", "require body is array of string.");
   }
-  const infos: info[] = [];
+  const infos: Info[] = [];
   for (const url of urls) {
     infos.push(await analyzeImageUrl(url));
   }
---
+++ functions/src/utility.ts
@@ -0,0 +1,65 @@
+import { protos } from "@google-cloud/vision";
+import { RGBColor } from "color-diff";
+import { assertIsDefined } from "./assert";
+import _ from "lodash";
+import IVertex = protos.google.cloud.vision.v1.IVertex;
+import { boxLine } from "intersects";
+
+export interface Info {
+  url: string;
+  title: string;
+  designId: string;
+  dominantColor: string;
+  designType: string;
+  authorName: string;
+  authorId: string;
+  islandName: string;
+}
+
+export interface Rect {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export interface Line {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+export function toRGBColor(c: [number, number, number]): RGBColor {
+  return { R: c[0], G: c[1], B: c[2] };
+}
+
+export function toRect(vertices?: IVertex[] | null): Rect {
+  assertIsDefined(vertices);
+  const left = _(vertices)
+    .map(v => v.x)
+    .min();
+  const right = _(vertices)
+    .map(v => v.x)
+    .max();
+  const top = _(vertices)
+    .map(v => v.y)
+    .min();
+  const bottom = _(vertices)
+    .map(v => v.y)
+    .max();
+  assertIsDefined(left);
+  assertIsDefined(right);
+  assertIsDefined(top);
+  assertIsDefined(bottom);
+  return {
+    x: left,
+    y: top,
+    w: right - left,
+    h: bottom - top,
+  };
+}
+
+export function intersect(rect: Rect, line: Line): boolean {
+  return boxLine(rect.x, rect.y, rect.w, rect.h, line.x1, line.y1, line.x2, line.y2);
+}
