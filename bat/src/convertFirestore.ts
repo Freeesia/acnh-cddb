de133bf1ff7931638125061e2bea04bf83427a8a
Freesia <freesia@studiofreesia.com>
2020-06-08 18:28:59 +0900

    usersからcontributorsに移動

--- bat/src/convertFirestore.ts
+++ bat/src/convertFirestore.ts
@@ -4,12 +4,9 @@ initializeApp();
 
 export default async function convertFirestore() {
   const db = firestore();
-  const designRef = db.collection("designs");
-  const ss = await designRef.get();
-  for (const doc of ss.docs) {
-    const data = doc.data();
-    const imageUrl = (data.imageUrl as string).replace("?name=large", "");
-    console.log(imageUrl);
-    await doc.ref.update({ imageUrl });
+  const users = (await db.collection("users").get()).docs;
+  const contributorsRef = db.collection("contributors");
+  for (const doc of users) {
+    await contributorsRef.doc(doc.id).create(doc.data());
   }
 }
