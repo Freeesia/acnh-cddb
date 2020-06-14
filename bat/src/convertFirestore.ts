2662d7b7115b2f51e21166cd97dd16574a69d372
Freesia <freesia@studiofreesia.com>
2020-06-15 00:58:03 +0900

    post.platformにTwitter入れる

--- bat/src/convertFirestore.ts
+++ bat/src/convertFirestore.ts
@@ -1,12 +1,17 @@
 import { firestore, initializeApp } from "firebase-admin";
 
+const path = "post.platform";
+
 initializeApp();
 
 export default async function convertFirestore() {
   const db = firestore();
-  const users = (await db.collection("users").get()).docs;
-  const contributorsRef = db.collection("contributors");
-  for (const doc of users) {
-    await contributorsRef.doc(doc.id).create(doc.data());
+  const designsRef = db.collection("designs");
+  const designs = await designsRef.get();
+  for (const doc of designs.docs) {
+    const platform = doc.get(path);
+    if (!platform) {
+      await doc.ref.update(path, "Twitter");
+    }
   }
 }
