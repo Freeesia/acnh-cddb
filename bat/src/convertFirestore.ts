e11538e0f9fd06579140c99c49925798b5ad326d
Freesia <freesia@studiofreesia.com>
2020-06-20 00:10:44 +0900

    Instagramの画像URLのフォーマット変更

--- bat/src/convertFirestore.ts
+++ bat/src/convertFirestore.ts
@@ -4,16 +4,16 @@ initializeApp();
 
 export default async function convertFirestore() {
   const db = firestore();
-  const designsRef = db.collection("designs").where("post.platform", "==", "Twitter");
+  const designsRef = db.collection("designs").where("post.platform", "==", "Instagram");
   const designs = await designsRef.get();
   for (const doc of designs.docs) {
     const imageUrl = doc.get("imageUrl") as string;
+    const thumbUrl = doc.get("thumbUrl") as string;
     await doc.ref.set(
       {
         imageUrls: {
-          thumb1: imageUrl + "?name=thumb",
-          thumb2: imageUrl + "?name=small",
-          large: imageUrl + "?name=large",
+          thumb2: thumbUrl,
+          large: imageUrl,
         },
       },
       { merge: true }
