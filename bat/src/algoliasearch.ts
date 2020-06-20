729a89a3043f8653eed39b732d0957f8c1ba1dd6
Freesia <freesia@studiofreesia.com>
2020-06-20 21:41:08 +0900

    機能をcoreに移動

--- bat/src/algoliasearch.ts
+++ bat/src/algoliasearch.ts
@@ -1,39 +1,9 @@
 import { designsRef } from "./firestore";
-import algoliasearch from "algoliasearch";
-import { assertIsDefined } from "@core/utilities/assert";
 import { DesignInfo } from "@core/models/types";
-import { Timestamp } from "@google-cloud/firestore";
-import _ from "lodash";
-const algoliaId = process.env.ALGOLIA_ID;
-const algoliaKey = process.env.ALGOLIA_KEY;
-assertIsDefined(algoliaId);
-assertIsDefined(algoliaKey);
-const client = algoliasearch(algoliaId, algoliaKey);
-const index = client.initIndex("designs");
-
-function adjunstInfo(info: DesignInfo) {
-  const data = _.cloneDeep(info) as any;
-  const id = info.designId;
-  data.createdAt = (info.createdAt as Timestamp).seconds;
-  delete data.imageUrl;
-  delete data.thumbUrl;
-  delete data.dominantColors;
-  delete data.post.contributor;
-  delete data.post.fromSwitch;
-  delete data.post.postId;
-  data.objectID = id;
-  return data;
-}
+import { adjunstInfo, designsIndex } from "@core/utilities/algolia";
 
 export async function batchAll() {
   const designs = await designsRef.get();
-
   const data = designs.docs.map(doc => adjunstInfo(doc.data() as DesignInfo));
-
-  await index.saveObjects(data, { autoGenerateObjectIDIfNotExist: false });
-}
-
-export async function postAlgolia(info: DesignInfo) {
-  const data = adjunstInfo(info);
-  await index.saveObject(data, { autoGenerateObjectIDIfNotExist: false });
+  await designsIndex.saveObjects(data, { autoGenerateObjectIDIfNotExist: false });
 }
--- core/src/utilities/algolia.ts
+++ core/src/utilities/algolia.ts
@@ -1,9 +1,27 @@
 import algoliasearch from "algoliasearch";
-import { assertIsDefined } from "./assert";
-
-const algoliaId = process.env.VUE_APP_ALGOLIA_ID;
-const algoliaKey = process.env.VUE_APP_ALGOLIA_KEY;
-assertIsDefined(algoliaId);
-assertIsDefined(algoliaKey);
+import { DesignInfo } from "../models/types";
+import { Timestamp } from "@google-cloud/firestore";
+import _ from "lodash";
+const algoliaId = process.env.VUE_APP_ALGOLIA_ID ?? process.env.ALGOLIA_ID ?? "";
+const algoliaKey = process.env.VUE_APP_ALGOLIA_KEY ?? process.env.ALGOLIA_KEY ?? "";
 const algoliaClient = algoliasearch(algoliaId, algoliaKey);
 export const designsIndex = algoliaClient.initIndex("designs");
+
+export function adjunstInfo(info: DesignInfo) {
+  const data = _.cloneDeep(info) as any;
+  const id = info.designId;
+  data.createdAt = (info.createdAt as Timestamp).seconds;
+  delete data.imageUrl;
+  delete data.thumbUrl;
+  delete data.dominantColors;
+  delete data.post.contributor;
+  delete data.post.fromSwitch;
+  delete data.post.postId;
+  data.objectID = id;
+  return data;
+}
+
+export async function postAlgolia(info: DesignInfo) {
+  const data = adjunstInfo(info);
+  await designsIndex.saveObject(data, { autoGenerateObjectIDIfNotExist: false });
+}
