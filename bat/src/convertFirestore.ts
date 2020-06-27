c4262c2c8ec693003d7fbf89e162565c65601c9e
Freesia <freesia@studiofreesia.com>
2020-06-27 20:22:00 +0900

    contoributorsのTwitterアカウントの変換

--- bat/src/convertFirestore.ts
+++ bat/src/convertFirestore.ts
@@ -1,22 +1,30 @@
 import { firestore, initializeApp } from "firebase-admin";
+import DocRef = firestore.DocumentReference;
+import { Contributor } from "@core/models/types";
 
 initializeApp();
 
 export default async function convertFirestore() {
   const db = firestore();
-  const designsRef = db.collection("designs").where("post.platform", "==", "Instagram");
+  const contributorsRef = db.collection("contributors");
+  const designsRef = db.collection("designs").where("post.platform", "==", "Twitter");
   const designs = await designsRef.get();
   for (const doc of designs.docs) {
-    const imageUrl = doc.get("imageUrl") as string;
-    const thumbUrl = doc.get("thumbUrl") as string;
-    await doc.ref.set(
-      {
-        imageUrls: {
-          thumb2: thumbUrl,
-          large: imageUrl,
-        },
-      },
-      { merge: true }
-    );
+    const oldConRef = doc.get("post.contributor") as DocRef;
+    if (oldConRef.id.startsWith("Twitter")) {
+      continue;
+    }
+    const conDoc = await oldConRef.get();
+    if (conDoc.exists) {
+      const contributor = conDoc.data() as Contributor;
+      contributor.platform = "Twitter";
+      const newConRef = contributorsRef.doc(`${contributor.platform}:${contributor.id}`);
+      await newConRef.set(contributor);
+      await oldConRef.delete();
+      await doc.ref.update("post.contributor", newConRef);
+    } else {
+      const newConRef = contributorsRef.doc(`Twitter:${conDoc.id}`);
+      await doc.ref.update("post.contributor", newConRef);
+    }
   }
 }
