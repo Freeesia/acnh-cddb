0fdbaf7ec8a0c8a890c3e18a7da8bf279aa4cb75
Freesia <freesia@studiofreesia.com>
2020-06-29 00:12:31 +0900

    サインイン画面変更

--- hosting/src/views/Signin.vue
+++ hosting/src/views/Signin.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="auth-container"></div>
+  <v-container>
+    <h1>
+      <p class="text-center text-h4">あつまれ マイデザの🌳</p>
+      <p class="text-center text-h5">へ ようこそ</p>
+    </h1>
+    <section class="pa-6">
+      <p class="text-center">あつまれ マイデザの🌳はあつ森のマイデザが集まっているサービスです</p>
+    </section>
+    <section class="pa-6">
+      <p class="text-center"><router-link to="/tos">利用規約</router-link>に同意してログイン↓</p>
+      <div id="auth-container"></div>
+    </section>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -23,6 +35,7 @@ export default class Login extends Vue {
     }
     ui.start("#auth-container", {
       signInOptions: [auth.TwitterAuthProvider.PROVIDER_ID],
+      signInFlow: "popup",
       callbacks: {
         signInSuccessWithAuthResult: this.signInSuccess,
       },
