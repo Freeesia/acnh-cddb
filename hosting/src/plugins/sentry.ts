b27e1c07ebd283d7284af97f3386c9f3dd76d2fb
Freesia <freesia@studiofreesia.com>
2020-07-05 04:44:12 +0900

    クラッシュレポートの送信 close #27

---
+++ hosting/src/plugins/sentry.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://61a8039368bf423190c6cecbb0a126ea@o351180.ingest.sentry.io/5309536",
+    integrations: [new VueIntegration({ Vue, attachProps: true })],
+  });
+}
