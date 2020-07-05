97c6ac0bbb64802f18941436caafb3bdc158f81a
Freesia <freesia@studiofreesia.com>
2020-07-05 23:06:45 +0900

    ダウンロード済み用フィールド追加  #15

--- bat/src/convertFirestore.ts
+++ bat/src/convertFirestore.ts
@@ -1,21 +1,17 @@
 import { firestore, initializeApp } from "firebase-admin";
-import DocRef = firestore.DocumentReference;
-import { Contributor } from "@core/models/types";
 
 initializeApp();
 
 export default async function convertFirestore() {
   const db = firestore();
-  const contributorsRef = db.collection("contributors");
-  const contributors = await contributorsRef.listDocuments();
-  for (const docRef of contributors) {
-    if (docRef.id.includes(":")) {
-      continue;
-    }
-    const doc = await docRef.get();
-    const contributor = doc.data() as Contributor;
-    contributor.platform = "Twitter";
-    await docRef.delete();
-    await contributorsRef.doc(`${contributor.platform}:${contributor.id}`).set(contributor);
+  const usersRef = db.collection("users");
+  const userRefs = await usersRef.listDocuments();
+  for (const docRef of userRefs) {
+    await docRef.set(
+      {
+        downloaded: [],
+      },
+      { merge: true }
+    );
   }
 }
--- bat/src/index.ts
+++ bat/src/index.ts
@@ -1,15 +1,9 @@
-import { searchTweets } from "./twitter";
-import { searchPosts } from "./instagram";
 import { batchAll } from "./algoliasearch";
 import convertFirestore from "./convertFirestore";
-import { deleteDeadRef } from "./deleteDeadRef";
 
 (async () => {
   console.log("Start");
-  // await convertFirestore();
-  await deleteDeadRef();
-  await searchPosts();
-  await searchTweets();
+  await convertFirestore();
   // await batchAll();
   console.log("Finish");
 })().catch(error => {
