0e10055b9f55cc9ab8ca6680c69b3e85d9de3ad2
Freesia <freesia@studiofreesia.com>
2020-07-25 14:24:15 +0900

    ログ証入

--- bat/src/collect.ts
+++ bat/src/collect.ts
@@ -5,9 +5,13 @@ import { backup, deleteOld } from "./backup";
 
 (async () => {
   console.log("Start");
+  console.log("バックアップ処理");
   await Promise.all([backup(), deleteOld()]);
+  console.log("参照切れ削除");
   await deleteDeadRef();
+  console.log("Instagram検索");
   await searchPosts();
+  console.log("Twitter検索");
   await searchTweets();
   console.log("Finish");
 })().catch(error => {
