2855ab347baacf4a749462d0f6f8d3a5908cfa22
Freesia <freesia@studiofreesia.com>
2020-07-25 16:06:19 +0900

    削除処理の改善

--- bat/src/collect.ts
+++ bat/src/collect.ts
@@ -5,14 +5,14 @@ import { backup, deleteOld } from "./backup";
 
 (async () => {
   console.log("Start");
-  console.log("バックアップ処理");
-  await Promise.all([backup(), deleteOld()]);
+  // console.log("バックアップ処理");
+  // await Promise.all([backup(), deleteOld()]);
   console.log("参照切れ削除");
   await deleteDeadRef();
-  console.log("Instagram検索");
-  await searchPosts();
-  console.log("Twitter検索");
-  await searchTweets();
+  // console.log("Instagram検索");
+  // await searchPosts();
+  // console.log("Twitter検索");
+  // await searchTweets();
   console.log("Finish");
 })().catch(error => {
   console.error(error);
