211dc5e5a616496e410c3d3d7249f9d372b56b22
Freesia <freesia@studiofreesia.com>
2020-08-30 00:31:11 +0900

    古いデータを強制的に削除

--- bat/src/index.ts
+++ bat/src/index.ts
@@ -1,15 +1,18 @@
 import { batchAll, refreshTags } from "./algoliasearch";
 import convertFirestore from "./convertFirestore";
 import { backup } from "./backup";
+import { deleteDeadDesignRef, deleteDeadDreamRef } from "./deleteDeadRef";
 
 (async () => {
   console.log("Start");
-  // console.log("バックアップ処理");
-  // await backup();
-  console.log("firestore処理");
-  await convertFirestore();
+  console.log("バックアップ処理");
+  await backup();
+  // console.log("firestore処理");
+  // await convertFirestore();
   // await batchAll();
   // await refreshTags();
+  await deleteDeadDesignRef(true);
+  await deleteDeadDreamRef(true);
   console.log("Finish");
 })().catch(error => {
   console.error(error);
