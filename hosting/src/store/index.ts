7b052028cec005a7929bdb91fcd54342f471aa3c
Freesia <freesia@studiofreesia.com>
2020-09-20 03:17:28 +0900

    authもvuex-class-componentに変更 ただしvuexfireが動かない

--- hosting/src/store/index.ts
+++ hosting/src/store/index.ts
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { vuexfireMutations } from "vuexfire";
-import { getModule } from "vuex-module-decorators";
 import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
 import general from "./modules/general";
@@ -11,7 +10,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    auth,
+    ...extractVuexModule(auth),
     ...extractVuexModule(general),
   },
   mutations: {
@@ -24,6 +23,6 @@ const store = new Vuex.Store({
   ],
 });
 
-export const AuthModule = getModule(auth, store);
+export const AuthModule = createProxy(store, auth);
 export const GeneralModule = createProxy(store, general);
 export default store;
--- hosting/src/store/modules/auth.ts
+++ hosting/src/store/modules/auth.ts
@@ -1,10 +1,15 @@
-import { VuexModule, Module, Mutation, Action } from "vuex-module-decorators";
 import { auth, User } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import FirestoreAction, { FirestoreActionContext } from "@/modules/vuexfire-decorator";
 import { DesignList } from "@core/models/types";
 import { usersRef, designListsRef } from "@/plugins/firestore";
+import { action, createModule, getRawActionContext } from "vuex-class-component";
+
+const VuexModule = createModule({
+  namespaced: "auth",
+  strict: false,
+});
 
 interface UserInfo {
   favs: string[];
@@ -12,40 +17,44 @@ interface UserInfo {
   dreamFavs: string[];
 }
 
-@Module({ namespaced: true, name: "auth" })
 export default class Auth extends VuexModule {
   user: User | null = null;
-  info: UserInfo | null = null;
-  lists: DesignList[] = [];
+  _info: UserInfo | null = null;
+  _lists: DesignList[] = [];
 
-  @Mutation
-  private setUser(user: User | null) {
-    this.user = user;
+  get info() {
+    return this._info;
   }
 
-  @Action
+  get lists() {
+    return this._lists;
+  }
+
+  @action
   async signOut() {
     await auth().signOut();
-    this.context.commit("setUser", null);
+    this.user = null;
   }
 
-  @Action
+  @action({ mode: "raw" })
   @FirestoreAction()
-  init(user: User | null) {
+  init() {
+    const context = getRawActionContext(this);
     // Refactor: this line is kind of danger, so it should be refactored
-    const { bindFirestoreRef, unbindFirestoreRef } = this.context as FirestoreActionContext<any, any>;
-
+    const { bindFirestoreRef, unbindFirestoreRef } = context as FirestoreActionContext<any, any>;
+    const user = this.user;
     if (user) {
       return Promise.all([
         bindFirestoreRef("info", usersRef.doc(user.uid), { maxRefDepth: 0 }),
         bindFirestoreRef("lists", designListsRef.where("owner", "==", user.uid), { maxRefDepth: 0 }),
       ]);
     } else {
-      return unbindFirestoreRef("info");
+      unbindFirestoreRef("info");
+      return Promise.resolve();
     }
   }
 
-  @Action
+  @action
   async isSignedIn() {
     let user = this.user;
     if (!user) {
@@ -55,8 +64,8 @@ export default class Auth extends VuexModule {
       if (!user) {
         auth().signOut();
       }
-      this.context.commit("setUser", user);
-      this.context.dispatch("init", user);
+      this.user = user;
+      this.init();
     }
     return user ? true : false;
   }
